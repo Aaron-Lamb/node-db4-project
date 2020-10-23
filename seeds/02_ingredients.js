ecbb867d5a78669c0b69833b702e7ec979c8bfcb
Aaron Mendez <58440880+Aaron-Lamb@users.noreply.github.com>
2020-10-22 18:16:02 -0700

    Seeds working

--- seeds/02_ingredients.js
+++ seeds/02_ingredients.js
@@ -1,13 +1,9 @@
 
-exports.seed = function(knex) {
-  // Deletes ALL existing entries
-  return knex('table_name').del()
-    .then(function () {
-      // Inserts seed entries
-      return knex('table_name').insert([
-        {id: 1, colName: 'rowValue1'},
-        {id: 2, colName: 'rowValue2'},
-        {id: 3, colName: 'rowValue3'}
-      ]);
-    });
+exports.seed = async function(knex) {
+  await knex("ingredients").insert([
+    {name: "Eggs", amount: 3},
+    {name: "Bacon sliced", amount: 3},
+    {name: "Tablespoon of butter", amount: 1},
+    {name: "Salt and Pepper", amount: 0.32}
+  ])
 };
--- seeds/03_steps.js
+++ seeds/03_steps.js
@@ -1,13 +1,13 @@
 
 exports.seed = function(knex) {
-  // Deletes ALL existing entries
-  return knex('table_name').del()
-    .then(function () {
-      // Inserts seed entries
-      return knex('table_name').insert([
-        {id: 1, colName: 'rowValue1'},
-        {id: 2, colName: 'rowValue2'},
-        {id: 3, colName: 'rowValue3'}
+      return knex('steps').insert([
+        {step_number: 1, instructions: 'Break eggs into bowl, salt and pepper', recipe_id: 3},
+        {step_number: 2, instructions: 'Pour into buttered hot pan', recipe_id: 3},
+        {step_number: 3, instructions: 'Stir until cooked and serve', recipe_id: 3},
+        {step_number: 1, instructions: 'Place bacon in hot pan', recipe_id: 1},
+        {step_number: 2, instructions: 'Flip every few minutes until cooked', recipe_id: 1},
+        {step_number: 1, instructions: 'Break eggs into buttered pan, salt and pepper', recipe_id: 2},
+        {step_number: 2, instructions: 'Flip when whites are cooked and leave for 1 minute', recipe_id: 2},
+        {step_number: 3, instructions: 'Roll onto plate, bread, or other method of eating.', recipe_id: 2}
       ]);
-    });
 };
